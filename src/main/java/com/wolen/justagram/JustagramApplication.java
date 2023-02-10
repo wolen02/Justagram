c24d498de58a9bf4c774c3e8f6004fea48534d77
주지성\wolen <wnwltjd333@naver.com>
2023-02-11 04:18:14 +0900

    [주지성] -최초파일 업로드

---
+++ src/main/java/com/wolen/justagram/JustagramApplication.java
@@ -0,0 +1,13 @@
+package com.wolen.justagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolen/justagram/JustagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolen.justagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
